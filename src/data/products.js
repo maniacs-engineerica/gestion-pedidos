e7db1adfe6c5c0c30565a1e9431eb05695ba05df
Constanza <cotycirion94@gmail.com>
2020-06-22 16:36:34 -0300

    agrego url de las imagenes de los productos

--- src/data/products.js
+++ src/data/products.js
@@ -5,8 +5,8 @@ export default [
         slug: 'lemon-pie',
         description: 'Masa sablé dulce, con crema de limón y merengue italiano.',
         price: 20,
-        image: 'lemon-pie.jpg',
-        rating: 3
+        image: 'https://66.media.tumblr.com/b088b81078c168857d83527969147169/tumblr_nhlfjhDKgJ1sn5m44o1_500.jpg',
+        rating: 4
     },
     {
         id: 2,
@@ -14,8 +14,8 @@ export default [
         slug: 'chocotorta',
         description: 'Torta de chocolinas con dulce de leche y queso crema.',
         price: 25,
-        image: 'chocotorta.jpg',
-        rating: 4
+        image: 'https://i.pinimg.com/564x/0c/3e/a7/0c3ea73335cae2d23e0cccf7317ec4c9.jpg',
+        rating: 3
     },
     {
         id: 3,
@@ -23,7 +23,7 @@ export default [
         slug: 'cheesecake',
         description: 'Cheesecake con base crocante, crema de limon y frutos rojos.',
         price: 30,
-        image: 'cheesecake.jpg',
+        image: 'https://images.pexels.com/photos/4040693/pexels-photo-4040693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
         rating: 5
     },
     {
@@ -32,7 +32,16 @@ export default [
         slug: 'pan-dulce',
         description: 'Soy un pan dulce.',
         price: 18,
-        image: 'pan-dulce.jpg',
+        image: 'https://i.pinimg.com/564x/b1/ab/99/b1ab99774f44b1e4738cb83a18a920d6.jpg',
+        rating: 3
+    },
+    {
+        id: 5,
+        name: 'Brownie',
+        slug: 'brownie',
+        description: 'Soy un brownie.',
+        price: 20,
+        image: 'https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
         rating: 4
     }
 ]
